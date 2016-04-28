1977ad9479bf38e9368b67bc77ae4fa54f49b7c3
Scott Stewart <ScottInColorado@gmail.com>
2016-04-28 13:37:27 -0600

    added tests/ directory with a simple test that should pass one test of 'nosetests' run from permamodel/permamodel/

---
+++ permamodel/tests/test_frost_number.py
@@ -0,0 +1,11 @@
+"""
+test_frost_number.py
+  tests of the frost_number component of permamodel
+"""
+
+def test_testing():
+    # This should pass as long as this routine is getting called
+    assert(True)
+
+
+
