39a743463f55c3cfbbea05b4d471d01f66dd93f8
Mark Piper <mark.piper@colorado.edu>
2017-02-07 10:51:37 -0700

    Add unit tests for all package directories

--- permamodel/tests/test_perma_base.py
+++ permamodel/tests/test_perma_base.py
@@ -3,11 +3,39 @@ test_perma_base.py
   tests of the perma_base component of permamodel
 """
 
-from permamodel.components import frost_number
 import os
-import numpy as np
-from .. import permamodel_directory, data_directory, examples_directory
+from nose.tools import assert_true
+from .. import (permamodel_directory, data_directory,
+                examples_directory, tests_directory)
 
-def test_directory_names_are_set():
+
+def test_permamodel_directory_is_set():
     assert(permamodel_directory is not None)
 
+
+def test_data_directory_is_set():
+    assert(data_directory is not None)
+
+
+def test_examples_directory_is_set():
+    assert(examples_directory is not None)
+
+
+def test_tests_directory_is_set():
+    assert(tests_directory is not None)
+
+
+def test_permamodel_directory_exists():
+    assert_true(os.path.isdir(permamodel_directory))
+
+
+def test_data_directory_exists():
+    assert_true(os.path.isdir(data_directory))
+
+
+def test_examples_directory_exists():
+    assert_true(os.path.isdir(examples_directory))
+
+
+def test_tests_directory_exists():
+    assert_true(os.path.isdir(tests_directory))
