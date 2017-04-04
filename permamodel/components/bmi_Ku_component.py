a02f8ad128f2e5570806becfe109016474e78ab5
Mark Piper <mark.piper@colorado.edu>
2017-04-04 15:14:15 -0600

    Remove call to get_var_grid
    This is the same bug that was in the FrostNumberModel BMI.

--- permamodel/components/bmi_Ku_component.py
+++ permamodel/components/bmi_Ku_component.py
@@ -412,4 +412,4 @@ class BmiKuMethod( perma_base.PermafrostComponent ):
         int
             Rank of grid.
         """
-        return len(self.get_grid_shape(self.get_var_grid(var_id)))
+        return len(self.get_grid_shape(var_id))
