b6548c25f8da155664ac2b95a7e0ab89f9775eb3
Kang <wk1984@users.noreply.github.com>
2017-04-06 10:37:56 -0600

    add license.

--- permamodel/components/bmi_Ku_component.py
+++ permamodel/components/bmi_Ku_component.py
@@ -1,5 +1,29 @@
 # -*- coding: utf-8 -*-
-"""  Frost Number by Nelson and Outcalt 1983. DOI: 10.2307/1551363. http://www.jstor.org/stable/1551363
+"""  Kudryavtsev model by Anisimov et al. (1997). 
+
+Anisimov, O. A., Shiklomanov, N. I., & Nelson, F. E. (1997).
+Global warming and active-layer thickness: results from transient general circulation models. 
+Global and Planetary Change, 15(3-4), 61-77. DOI:10.1016/S0921-8181(97)00009-X
+
+*The MIT License (MIT)*
+Copyright (c) 2016 permamodel
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*
+
 """
 
 import numpy as np
@@ -11,7 +35,7 @@ from permamodel.components import Ku_method
 import os
 
 """
-class BmiKuMethod( frost_number.BmiFrostnumberMethod ):
+class BmiKuMethod( perma_base.PermafrostComponent ):
     _thisname = 'this name'
 """
 
