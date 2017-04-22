950f08371061ceb0e1635d87077e6f0d2e6708b6
Scott <scotts@nsidc.org>
2017-04-22 12:50:28 -0600

    copied np.stack() definition from NumPy1.10 to frost_number_Geo.py as np_stack() so that datacube works

--- permamodel/components/frost_number_Geo.py
+++ permamodel/components/frost_number_Geo.py
@@ -305,9 +305,81 @@ class FrostnumberGeoMethod( perma_base.PermafrostComponent ):
         for n in range(ngridlines):
             config_arg = eval(config['%s_grid_data_%d' % (gridname, n)])
             slicelist.append(np.array(config_arg))
-        datacube = np.stack(slicelist)
+        # In NumPy 1.10 later, we can use np.stack()
+        # But for v1.09 -- which pip seems to install -- we need np.dstack()
+        # datacube = np.stack(slicelist)
+        print("slicelist:")
+        print(slicelist)
+        datacube = self.np_stack(slicelist)
 
         return datelist, datacube
+
+    def np_stack(self, arrays, axis=0):
+        """
+        In order to provide this function from numpy v1.10, this is copied
+        from:
+           https://github.com/numpy/numpy/blob/
+           f4cc58c80df5202a743bddd514a3485d5e4ec5a4/numpy/core/shape_base.py
+        Join a sequence of arrays along a new axis.
+        The `axis` parameter specifies the index of the new axis in the dimensions
+        of the result. For example, if ``axis=0`` it will be the first dimension
+        and if ``axis=-1`` it will be the last dimension.
+        .. versionadded:: 1.10.0
+        Parameters
+        ----------
+        arrays : sequence of array_like
+            Each array must have the same shape.
+        axis : int, optional
+            The axis in the result array along which the input arrays are stacked.
+        Returns
+        -------
+        stacked : ndarray
+            The stacked array has one more dimension than the input arrays.
+        See Also
+        --------
+        concatenate : Join a sequence of arrays along an existing axis.
+        split : Split array into a list of multiple sub-arrays of equal size.
+        Examples
+        Note: Replace '>>>' with '>>' so it doesn't doctest
+        --------
+        >> arrays = [np.random.randn(3, 4) for _ in range(10)]
+        >> np.stack(arrays, axis=0).shape
+        (10, 3, 4)
+        >> np.stack(arrays, axis=1).shape
+        (3, 10, 4)
+        >> np.stack(arrays, axis=2).shape
+        (3, 4, 10)
+        >> a = np.array([1, 2, 3])
+        >> b = np.array([2, 3, 4])
+        >> np.stack((a, b))
+        array([[1, 2, 3],
+               [2, 3, 4]])
+        >> np.stack((a, b), axis=-1)
+        array([[1, 2],
+               [2, 3],
+               [3, 4]])
+        """
+        arrays = [np.asanyarray(arr) for arr in arrays]
+        if not arrays:
+            raise ValueError('need at least one array to stack')
+
+        shapes = set(arr.shape for arr in arrays)
+        if len(shapes) != 1:
+            raise ValueError('all input arrays must have the same shape')
+
+        result_ndim = arrays[0].ndim + 1
+        if not -result_ndim <= axis < result_ndim:
+            msg = 'axis {0} out of bounds [-{1}, {1})'.format(axis, result_ndim)
+            raise IndexError(msg)
+        if axis < 0:
+            axis += result_ndim
+
+        sl = (slice(None),) * axis + (np.newaxis,)
+        expanded_arrays = [arr[sl] for arr in arrays]
+
+        # Changed name from that of copied code
+        # return _nx.concatenate(expanded_arrays, axis=axis)
+        return np.concatenate(expanded_arrays, axis=axis)
 
     def get_datacube_slice(self, thisdate, datacube, cubedates):
         # Data are invalid until the first date in the datacube
