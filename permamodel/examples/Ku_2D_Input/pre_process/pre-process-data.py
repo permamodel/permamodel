dd3fde92b64c431929bc1dc7f50cbe3714896f9b
Kang <Kang.Wang@colorado.edu>
2017-05-15 22:22:36 -0600

    prepare multi-year data
    produce multi-year data from monthly dataset.

--- permamodel/examples/Ku_2D_Input/pre_process/pre-process-data.py
+++ permamodel/examples/Ku_2D_Input/pre_process/pre-process-data.py
@@ -8,7 +8,7 @@ Created on Mon May 15 16:45:54 2017
 import numpy as np
 from netCDF4 import Dataset
 
-data_file = '_grib2netcdf-atls17-a562cefde8a29a7288fa0b8b7f9413f7-ts0Hg5.nc';
+data_file = '_grib2netcdf-atls00-a562cefde8a29a7288fa0b8b7f9413f7-A_NvIG.nc';
 mask_file = 'mask.nc'
 
 fid  = Dataset(mask_file)
@@ -26,3 +26,13 @@ t2m  = data.variables['t2m'][:] - 237.15
 t2m = np.reshape(t2m, (np.size(t2m,axis=0)/12,12,np.size(t2m,axis=1),np.size(t2m,axis=2)))
 
 ta  = np.mean(t2m, axis=1)
+
+aa  = 0.5 * (np.max(t2m, axis = 1) - np.min(t2m, axis = 1))
+
+sd  = data.variables['sd'][:] 
+
+sd = np.reshape(sd, (np.size(sd,axis=0)/12,12,np.size(sd,axis=1),np.size(sd,axis=2)))
+sd[np.where(sd==0.0)] = np.nan
+sd = np.nanmean(sd,axis=1)
+sd[np.where(sd==0.0)] = np.nan
+   
