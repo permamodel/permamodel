b97bbac1bae4bfdd30d10c6e231df5584e1fb03e
Mark Piper <mark.piper@colorado.edu>
2017-11-20 11:13:22 -0700

    Increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='permamodel',
-      version='0.1.0',
+      version='0.1.1',
       author='Elchin Jafarov and Scott Stewart',
       author_email='james.stewart@colorado.edu',
       description='Permamodel',
