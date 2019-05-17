e9b0c3e96b336022b9518898eddd2bc478f7e50c
wk1984 <waccount1984@gmail.com>
2019-05-17 16:27:14 -0600

    create a new method of Ku model
    Start to moving Ku method to a new version, so-called KuFlex method.
    
    Objective: expose all variables and parameters.
                      more suitable for coulpling in PyMT.

---
+++ permamodel/examples/run_test_KuFlex.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+A simple BMI KuModel example. Run with:
+
+  $ python -m permamodel.tests.run_test_BMI_ku_model
+
+Created on Tue Jan 10 10:56:16 2017
+@author: kangwang
+"""
+from __future__ import print_function
+
+import os
+#from permamodel.components import bmi_Ku_component
+from permamodel import examples_directory
+from permamodel.components import KuFlex_method
