be56b76e4d5bf483826a7cb8b54b2b049f978365
Mark Piper <mark.piper@colorado.edu>
2023-02-07 15:38:07 -0700

    Configure versioning with zest.releaser

--- permamodel/__init__.py
+++ permamodel/__init__.py
@@ -8,8 +8,12 @@ Provides:
 """
 
 import os
+from ._version import __version__
+
 
 permamodel_directory = os.path.dirname(__file__)
 data_directory = os.path.join(permamodel_directory, "data")
 examples_directory = os.path.join(permamodel_directory, "examples")
 tests_directory = os.path.join(permamodel_directory, "tests")
+
+__all__ = ["__version__"]
--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="permamodel",
-    version="0.1.2",
     author="Elchin Jafarov and Scott Stewart",
     author_email="james.stewart@colorado.edu",
     description="Permamodel",
