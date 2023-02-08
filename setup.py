3869493dc76f23c55b1cbb18e8b20d373fdecf18
Mark Piper <mark.piper@colorado.edu>
2023-02-07 18:21:16 -0700

    Move project metadata to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,22 +1,6 @@
-#! /usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
     name="permamodel",
-    author="Elchin Jafarov and Scott Stewart",
-    author_email="james.stewart@colorado.edu",
-    description="Permamodel",
-    long_description=open("README.md").read(),
-    packages=find_packages(),
-    # install_requires=('numpy', 'gdal', 'pyproj'),
-    install_requires=(
-        "affine",
-        "netCDF4",
-        "scipy",
-        "numpy",
-        "pyyaml",
-        "python-dateutil",
-    ),
-    package_data={"": ["examples/*.cfg", "examples/*.dat", "data/*"]},
 )
